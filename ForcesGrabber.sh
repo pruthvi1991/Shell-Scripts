abae65d1d1ac590921eb680d7335f007b40795c8
pruthvi1991 <pruthvi.krishnaaa@gmail.com>
2015-02-10 22:13:43 -0500

    Stamped bugs in ForceGrabber, Added extra functionality to mergeLogs

--- ForcesGrabber.sh
+++ ForcesGrabber.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+rm log.forces
+rm -r postProcessing
+
 endTime=$(grep -w 'endTime' ./system/controlDict | grep '[0-9]' | sed 's/;//g' | awk '{print$2}')	# Gets endTime value from controlDict
 intendTime=$(grep -w 'endTime' ./system/controlDict | grep '[0-9]' | sed 's/;//g' | awk '{print$2*1000}')       # Multiplying by 1000 to convert to integer in case endTime is float
-
+writeTime=$(grep -w 'writeInterval' ./system/controlDict | grep '[0-9]' | sed 's/;//g' | awk '{print$2}') # Gets writeInterval from controlDict
 execFlowFunctionObjects >> log.forces &		# Executes the function execFlowFunctionObjects and writes output to log.forces
 
 sleep 5
@@ -10,16 +13,17 @@ sleep 5
 presentTime=$(tail -22 log.forces | grep Time | awk '{print$3}' | awk NR==1)	# Checks for many time steps the function ran and gets the last time value
 intpresentTime=$(tail -22 log.forces | grep Time | awk '{print$3*1000}' | awk NR==1)	# Converting a possible float to integer
 
-echo "endTime = $endTime, presentTime = $presentTime, Integer endTime = $intendTime, Integer presentTime = $intpresentTime"
+echo "endTime = $endTime, presentTime = $presentTime, Integer endTime = $intendTime, Integer presentTime = $intpresentTime, writeInterval = $writeTime"
 
 echo "Entering Loop at $presentTime"
 
 while [ $intpresentTime -lt $intendTime ];		# If the function failed to execute at some point this loop will make it start again
 do
-	startTime=$(python -c print "$presentTime + $endTime")
+#	startTime=$(python -c print " $presentTime + $writeTime ")  # Finds time from which the function must start again
+	startTime=$(echo "$presentTime+$writeTime" | bc)  # Finds time from which the function must start again
 	echo "Crashed at $presentTime, re-starting from $startTime"
-	execFlowFunctionObjects -time $presentTime: >> log.forces &
-	sleep 15	# Waits for 15 seconds before executing next command. This is necessary to ensure execFlowFunctionObjects is not re-run midway
+	execFlowFunctionObjects -time $startTime: >> log.forces &
+	sleep 10	# Waits for 15 seconds before executing next command. This is necessary to ensure execFlowFunctionObjects is not re-run midway
 	presentTime=$(tail -22 log.forces | grep Time | awk '{print$3}' | awk NR==1)
 	intpresentTime=$(tail -22 log.forces | grep Time | awk '{print$3*1000}' | awk NR==1)
 done
--- mergeLogs.sh
+++ mergeLogs.sh
@@ -2,5 +2,8 @@
 
 #filenames=$(ls | grep 'forces')
 #sed -i 's/[()]//g' forces*
+mkdir bins
+binNames=$(ls | grep bins)
+mv $binNames bins/
 filenames=$(ls | grep 'forces' | sed 's/[forces_]//g' | sort -n | sed 's/^/forces_/' | sed 's/forces_.dat/forces.dat/g')
 cat $filenames >> merged.dat
